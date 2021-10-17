e899d7f299a9c84234c130d35d9839419ddbabe3
panmissl <panmissl@163.com>
2021-10-17 23:02:56 +0800

    update

---
+++ src/pages/Index.jsx
@@ -0,0 +1,10 @@
+import { PageContainer } from '@ant-design/pro-layout';
+import React from 'react';
+
+const Index = () => (
+  <PageContainer>
+    欢迎光临
+  </PageContainer>
+);
+
+export default Index;
