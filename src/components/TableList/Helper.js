1c4c218ba25c55ae3a4f845b95fe84c38307136d
panmissl <panmissl@163.com>
2021-10-17 18:44:29 +0800

    update

--- src/components/TableList/Helper.js
+++ src/components/TableList/Helper.js
@@ -264,8 +264,8 @@ const parsePageInfo = ({ model, ellipsisFieldList = [], operationList = [], show
 
     const modelBigName = model.substr(model.lastIndexOf('.') + 1);
     //const modelSmallName = modelBigName.substr(0, 1).toLowerCase() + modelBigName.substr(1);
+    //const baseUrl = '/ajax/' + module.toLowerCase() + '/' + modelSmallName;
     const baseUrl = '/ajax/' + module.toLowerCase() + '/' + modelBigName.toLowerCase();
-    const baseUrl = '/ajax/' + module.toLowerCase() + '/' + modelSmallName;
     const listUrl = baseUrl + '/listData';
     const saveUrl = baseUrl + '/save';
     const deleteUrl = baseUrl + '/delete/';
