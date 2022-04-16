972bc9eded5eca70f7e2f02bda9f469ae056eb4c
panliang <panmissl@163.com>
2022-04-16 13:24:24 +0800

    update

--- src/pages/base/DataDict.jsx
+++ src/pages/base/DataDict.jsx
@@ -1,11 +1,31 @@
 import TableList from '@/components/TableList';
 import { PageContainer } from '@ant-design/pro-layout';
-import React from 'react';
+import { ReloadOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { message } from 'antd';
+import request from '@/utils/request';
 
 const Page = () => {
+
+  const [ loading, setLoading ] = useState(false);
+
+  const refreshCache =  async () => {
+    setLoading(true);
+    const hide = message.loading('刷新中...');
+    const success = await request(ctx + 'ajax/base/datadict/refreshCache');
+    hide();
+    setLoading(false);
+    if (success) {
+      message.success('刷新成功');
+    }
+  };
+
   return (
     <PageContainer>
-      <TableList model="com.cory.model.DataDict" showId={true} />
+      <TableList model="com.cory.model.DataDict" 
+        showId={true}
+        toolbar={[{label: '刷新缓存', handler: refreshCache, type: 'primary', danger: true, icon: <ReloadOutlined />, loading: loading}]}
+       />
     </PageContainer>
   );
 };
