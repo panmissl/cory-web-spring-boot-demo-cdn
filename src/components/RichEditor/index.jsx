c49ca0ae99d75eb036424585662cc5d7904e2368
panliang <panmissl@163.com>
2022-05-21 07:09:45 +0800

    update

--- src/components/RichEditor/index.jsx
+++ src/components/RichEditor/index.jsx
@@ -1,12 +1,11 @@
-import { message, Upload, Modal, Input } from 'antd';
-import { PictureFilled, UploadOutlined } from '@ant-design/icons';
+import { message, Upload } from 'antd';
+import { PictureFilled } from '@ant-design/icons';
 import BraftEditor from 'braft-editor';
 import 'braft-editor/dist/index.css';
 import { ContentUtils } from 'braft-utils';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 
 /**
- * 一般情况下不用直接使用此组件，已经集成在TableList里了，在后台的Model里的Field里设置richText=true即可。然后只需要在TableList的使用里设置uploadHandler即可。
  * 富文本编辑器，目前用的是：BRAFT EDITOR。文档：
  * https://gitcode.net/mirrors/margox/braft-editor
  * https://www.yuque.com/braft-editor/be/lzwpnr#zrs7hr
@@ -15,6 +14,10 @@ import { Fragment, useState } from 'react';
  * 用法：<RichEditor placeholder={xxx} uploadHandler={xx} />，参考TableList的用法
  * uploadHandler是一个函数，定义为: (file, callback) = {}, file为上传的文件体，自己可以log出来看结果，参考OssUploader，callback是回调函数，上传成功后需要调用此回调函数，参数为文件的访问url
  * uploadHandler在使用TableList时直接传入即可。一般可以用OssUploader导出的uploadToOss方法即可
+ * 
+ * 注意：
+ * 1、初始值不能直接用数据库存储的值，要用下面提供的方法：initRichEditorValue来初始化。
+ * 2、提交时，要用toRAW()或toHTML()方法提取内容。比如：values.content.toRAW() // or values.content.toHTML()
  */
 const RichEditor = props => {
 
@@ -27,8 +30,6 @@ const RichEditor = props => {
   }
 
   const [ html, setHtml ] = useState(newProps.value);
-  const [ showInternetImageModal, setShowInternetImageModal ] = useState(false);
-  const [ internetImageUrl, setInternetImageUrl ] = useState();
   delete newProps.value;
 
   let superOnChange = false;
@@ -97,25 +98,16 @@ const RichEditor = props => {
 
   const controls = [
     //'undo', 'redo', 'separator',
-    'fullscreen', 'headings', 'font-size', 'line-height', 'letter-spacing', 'separator',
+    'font-size', 'line-height', 'letter-spacing', 'separator',
     'text-color', 'bold', 'italic', 'underline', 'strike-through', 'hr', 'separator',
     //'superscript', 'subscript', 'remove-styles', 'emoji',  'separator', 
     'text-indent', 'text-align', 'separator',
     //'headings', 'list-ul', 'list-ol', 'blockquote', 'code', 'separator',
-    'list-ul', 'list-ol', 'blockquote', 'link', 'separator',
+    'headings', 'list-ul', 'list-ol', 'blockquote', 'link', 'separator',
     //'media', 'separator',
     //'clear'
-    'code',
   ];
   const extendControls = [{
-    key: 'internet-image',
-    type: 'component',
-    component: (
-      <button type="button" className="control-item button upload-button" data-title="插入网络图片" onClick={() => setShowInternetImageModal(true)}>
-        <PictureFilled style={{fontSize: '16px'}} />
-      </button>
-    )
-  }, {
     key: 'antd-uploader',
     type: 'component',
     component: (
@@ -126,8 +118,8 @@ const RichEditor = props => {
       >
         {/*<Button type='default' icon={<PictureFilled />}>插入图片</Button>*/}
         {/* 这里的按钮最好加上type="button"，以避免在表单容器中触发表单提交，用Antd的Button组件则无需如此 */}
-        <button type="button" className="control-item button upload-button" data-title="上传本地图片到OSS然后使用">
-          <UploadOutlined style={{fontSize: '16px'}} />
+        <button type="button" className="control-item button upload-button" data-title="插入图片">
+          <PictureFilled style={{fontSize: '16px'}} />
         </button>
       </Upload>
     )
@@ -138,30 +130,7 @@ const RichEditor = props => {
     <BraftEditor {...newProps} className={className} media={media} controls extendControls />
   );
   */
-  return (
-    <Fragment>
-      <BraftEditor {...newProps} className={className} controls={controls} extendControls={extendControls} value={html} onChange={htmlChange} />
-      <Modal title={false} visible={showInternetImageModal} closable={false} onOk={() => {
-        if (internetImageUrl && internetImageUrl.length > 0) {
-          if (html) {
-            setHtml(ContentUtils.insertMedias(html, [{
-              type: 'IMAGE',
-              url: internetImageUrl,
-            }]));
-          } else {
-            message.error('请将光标定位在编辑器内');
-          }
-        }
-        setShowInternetImageModal(false);
-        setInternetImageUrl(null);
-      }} onCancel={() => {
-        setShowInternetImageModal(false);
-        setInternetImageUrl(null);
-      }}>
-        <Input value={internetImageUrl} onChange={e => setInternetImageUrl(e.target.value)} placeholder= "请输入网络图片URL" />
-      </Modal>
-    </Fragment>
-  );
+  return <BraftEditor {...newProps} className={className} controls={controls} extendControls={extendControls} value={html} onChange={htmlChange} />;
 };
 
 export default RichEditor;
