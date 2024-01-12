14e6dc93cf216604edc161c26f611d37acf9df64
panliang <panmissl@163.com>
2024-01-12 08:02:21 +0800

    update

--- src/components/Captcha/index.jsx
+++ src/components/Captcha/index.jsx
@@ -1,9 +1,10 @@
-import { Input } from 'antd';
 import { useEffect, useState } from 'react';
+import './index.less';
 
 /**
  * 验证码组件
  * 用法：<Captcha onChange={v => setCaptchaText(v)} refresh={1} />
+ * css样式：可以用自定义样式覆盖默认样式
  * @param {*} props 
  *     onChange：输入验证码后的回调，输入完成后提交。提交的参数名叫：captcha。
  *     refresh：是一个数字，如果改变，那么会刷新验证码，实现不用点击图片，从外面刷新的功能
@@ -13,9 +14,8 @@ const Captcha = props => {
   const { onChange, refresh } = props;
 
   const [ version, setVersion ] = useState();
-  const [ captcha, setCapthca ] = useState();
+  const [ captcha, setCapthca ] = useState('');
 
-  useEffect(() => updateVersion(), []);
   useEffect(() => updateVersion(), [refresh]);
   
   const updateVersion = () => setVersion(new Date().getTime());
@@ -26,10 +26,9 @@ const Captcha = props => {
   };
 
   return (
-    <span style={{display: 'flex', flexDirection: 'row', alignItems: 'center', height: '40px', lineHeight: '40px'}}>
-      <span>验证码：</span>
-      <Input value={captcha} onChange={e => captchaChange(e.target.value)} style={{width: '80px'}} />
-      <img src={`/captcha.svl?v=${version}`} onClick={updateVersion} style={{marginLeft: '10px'}} />
+    <span className='captcha-container'>
+      <input placeholder="验证码" value={captcha || ''} onChange={e => captchaChange(e.target.value)} className='captcha-input' />
+      <img src={`/captcha.svl?v=${version}`} onClick={updateVersion} className='captcha-image' />
     </span>
   );
 };
